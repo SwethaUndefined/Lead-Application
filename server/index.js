7fca3f756b6dcbdbd99c1870b2613e5c7cc4d001
“Swetha”Undefined <ramyaragu407@gmail.com>
2024-08-11 13:11:58 +0530

    server

---
+++ server/index.js
@@ -0,0 +1,29 @@
+// server.js
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const cors = require('cors');
+
+const app = express();
+
+dotenv.config();
+const corsOptions = {
+    origin: 'http://localhost:3000', 
+    methods: 'GET,POST,PUT,DELETE', 
+    allowedHeaders: 'Content-Type,Authorization',
+  };
+  
+  app.use(cors(corsOptions));
+  
+
+connectDB();
+
+
+app.use(express.json());
+
+app.use('/api/leads', require('./routes/leadRoutes'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
